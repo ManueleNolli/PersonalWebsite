854d9c2461511c21a4a4ac9749a4e82dc423e833
ManueleNolli <manuele.nolli.01@gmail.com>
2024-08-06 10:48:59 +0200

    Update scroll

--- src/app/components/header/useHeader.ts
+++ src/app/components/header/useHeader.ts
@@ -11,11 +11,13 @@ export type MenuLabel = {
 export default function useHeader() {
   const [menuList, setMenuList] = useState<MenuLabel[]>([])
 
-  const findHeaderItems = () => {
-    return document.querySelectorAll('.header-item')
-  }
-
   useEffect(() => {
+    // helper to find header items
+    const findHeaderItems = () => {
+      return document.querySelectorAll('.header-item')
+    }
+
+    // fill menu list
     const fillMenuList = () => {
       const headerItems = findHeaderItems()
 
@@ -29,56 +31,50 @@ export default function useHeader() {
       setMenuList(newMenuList)
     }
 
-    fillMenuList()
-  }, [])
-
-  // update current menu according to scroll position
-  useEffect(() => {
+    // update current menu according to scroll position
     const updateCurrentMenu = () => {
       const currentScroll = window.scrollY
       const headerItems = findHeaderItems()
 
-      headerItems.forEach((item: Element) => {
-        const label = item.textContent
-        const url = `#${item.id}`
-        const element = document.querySelector(url)
+      let currentSection: string | null = null
+      let closestDistance = Infinity
 
+      headerItems.forEach((item: Element) => {
+        const element = document.querySelector(`#${item.id}`)
         if (!element) return
 
         const elementTop = element.getBoundingClientRect().top + window.scrollY
-        const elementBottom = elementTop + element.clientHeight
-        const elementCenter = elementBottom / 2
         const headerHeight = 80 // h-20 is 20rem = 80px
 
-        console.log('label', label)
-        console.log('currentScroll', currentScroll)
-        console.log('elementTop', elementTop)
-        console.log('elementBottom', elementBottom)
-        console.log('headerHeight', headerHeight)
-
-        // if the center of the element is the current scroll position #FIXME
-        const isCurrent = elementTop <= currentScroll + headerHeight && currentScroll + headerHeight < elementBottom
-
-        if (isCurrent) {
-          console.log('setting current menu', label)
-          setMenuList((prevMenuList) => {
-            return prevMenuList.map((menu) => {
-              if (menu.url === url) {
-                return { ...menu, isCurrent: true }
-              } else {
-                return { ...menu, isCurrent: false }
-              }
-            })
-          })
+        const distance = Math.abs(currentScroll - (elementTop - headerHeight))
+        if (distance < closestDistance) {
+          closestDistance = distance
+          currentSection = `#${item.id}`
         }
       })
+
+      if (currentSection) {
+        setMenuList((prevMenuList) =>
+          prevMenuList.map((menu) => ({
+            ...menu,
+            isCurrent: menu.url === currentSection,
+          }))
+        )
+      }
     }
 
+    // fill menu list
+    fillMenuList()
+
+    // calculate current menu
+    updateCurrentMenu()
+
+    // add event listener to update current menu
     window.addEventListener('scroll', updateCurrentMenu)
     return () => {
       window.removeEventListener('scroll', updateCurrentMenu)
     }
-  }, [menuList])
+  }, [])
 
   return {
     menuList,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="bg-red-200 h-96 text-center header-item scroll-mt-20" id="first">
+      <h1 className="bg-red-200 text-center header-item scroll-mt-20" id="first" style={{ height: 400 }}>
         First
       </h1>
-      <h1 className="bg-green-200 h-96 text-center header-item scroll-mt-20" id="second">
+      <h1 className="bg-green-200 h-96 text-center header-item scroll-mt-20" id="second" style={{ height: 250 }}>
         Second
       </h1>
-      <h1 className="bg-blue-200 h-96 text-center header-item scroll-mt-20" id="third">
+      <h1 className="bg-blue-200 h-96 text-center header-item scroll-mt-20" id="third" style={{ height: 100 }}>
         Third
       </h1>
-      <h1 className="bg-yellow-200 h-96 text-center header-item scroll-mt-20" id="fourth">
+      <h1 className="bg-yellow-200 h-96 text-center header-item scroll-mt-20" id="fourth" style={{ height: 900 }}>
         Fourth
       </h1>
     </div>
