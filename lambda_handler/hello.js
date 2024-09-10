ddc679481b22f735fac9647a0bc08dc517e7192c
ManueleNolli <manuele.nolli.01@gmail.com>
2024-09-10 16:59:31 +0200

    Update zip name

--- lambda_handler/hello.js
+++ lambda_handler/hello.js
@@ -2,6 +2,6 @@ exports.handler = async () => {
     return {
         statusCode: 200,
         headers: { "Content-Type": "text/plain" },
-        body: JSON.stringify({ message: `Hello, World from zip from S3!` }),
+        body: JSON.stringify({ message: `Hello, World from zip from S3 via github actions!` }),
     };
 };
