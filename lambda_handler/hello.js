667595df93da15f557b94e00b468746d38db71c0
ManueleNolli <manuele.nolli.01@gmail.com>
2024-09-13 17:09:24 +0200

    Update lambda handler

--- lambda_handler/hello.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async () => {
-    return {
-        statusCode: 200,
-        headers: { "Content-Type": "text/plain" },
-        body: JSON.stringify({ message: `Hello, World from zip from S3 via github actions!` }),
-    };
-};
